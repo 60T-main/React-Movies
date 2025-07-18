0b28d568d9c1957d7f1103b4ddc3ee1995bf3435
60T-Main <guga.nadirashvili12@gmail.com>
2025-07-19 00:10:18 +0400

    added movie cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import { useEffect, useState } from 'react';
 import Search from './components/Search.jsx';
+import Movie from './components/Movie.jsx';
+import Loader from './components/Loader.jsx';
+import { useDebounce } from 'react-use';
 
 const API_BASE_URL = 'https://api.themoviedb.org/3';
 
@@ -17,29 +20,56 @@ const App = () => {
 
   const [searchTerm, setSearchTerm] = useState('');
 
-  const [errorMessage,setErrorMessage] = useState('') 
+  const [errorMessage, setErrorMessage] = useState('');
+  
+  const [movieList, setMovieList] = useState([]);
 
-  const fetchMovies = async () => {
+  const [isLoading, setIsLoading] = useState(false);
+
+  // Debouncer is used to reduce search API calls to the server
+  const [debouncedSearchTerm, setDebouncedSearchTerm] = useState('');
+  useDebounce(() => setDebouncedSearchTerm(searchTerm), 700, [searchTerm]);
+
+  
+
+  const fetchMovies = async (query = '') => {
+    
+    setIsLoading(true);
+    setErrorMessage('');
+
     try {
-      const endpoint = '/discover/movie?sort_by=popularity.desc'; 
+
+      const endpoint = !query ? '/discover/movie?sort_by=popularity.desc' : `/search/movie?query=${encodeURIComponent(query)}`;
+      
       const response = await fetch(API_BASE_URL + endpoint, API_OPTIONS);
 
       if (!response.ok) {
         throw new Error('Failed to fetch movies');
       }
-      
+    
       const data = await response.json();
-      
+
+      if (data.Respose === 'False') {
+        setErrorMessage(data.Error || 'Failed to fetch movies');
+        setMovieList([]);
+        return;
+      }
+
+      setMovieList(data.results || [])
+    
     } catch (error) {
       console.error('Error fetching movies:', error);
       setErrorMessage('Error fetching movies. Please try again later...')
+    } finally {
+      setIsLoading(false)
     }
   }
+  
+    useEffect(() => {
+      fetchMovies(debouncedSearchTerm);
+    }, [debouncedSearchTerm]);
 
-  useEffect(() => {
-    fetchMovies();
-  }, []);
-
+  
 
   return ( 
     <main>
@@ -53,12 +83,23 @@ const App = () => {
         <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
         </header>
 
-        <section className='all-movies'>
+        <section className='all-movies pt-4'>
+          <h2 className='text-center'>All Movies</h2>
 
-          <h2>All Movies</h2>
-          
+          {isLoading ? (<Loader/>)
+            : errorMessage ? (
+              <p className='text-red-500'>{ errorMessage }</p>
+            )
+              : (
+                <ul>
+                {movieList.map((movie) => (
+                  <Movie key={movie.id} movie={movie} />
+                ))}
+                </ul>
+            ) 
+            
+            } 
 
-          {errorMessage && <p className='text-red-500'>{errorMessage}</p>}
         </section>
         
       </div>
---
+++ src/components/Loader.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+const Loader = () => {
+    return ( 
+        <div className='w-full flex justify-center items-center'><span className="loader text-center"></span></div>
+     );
+}
+ 
+export default Loader;
